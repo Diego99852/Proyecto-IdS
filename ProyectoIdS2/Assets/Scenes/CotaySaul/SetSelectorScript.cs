96673807768df113a889ec02804658c6612b1be3
elderdeveloper15 <54344130+elderdeveloper15@users.noreply.github.com>
2024-05-06 17:41:04 -0600

    segunda entrega

---
+++ ProyectoIdS2/Assets/Scenes/CotaySaul/SetSelectorScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetSelectorScript : MonoBehaviour
+{
+    public void FuckGoBack()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
