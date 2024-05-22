ffcca5822e07eaa60002cbe8dd9d350138dffe08
Diego99852 <124312009+Diego99852@users.noreply.github.com>
2024-05-21 20:01:22 -0600

    cuarto y funcionalidades extra

--- ProyectoIdS2/Assets/CotaySaul/scripts/movimientos.cs
+++ ProyectoIdS2/Assets/CotaySaul/scripts/movimientos.cs
@@ -3,19 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using UnityEngine.UI; // Necesario para manejar el botón
 
 public class movimientos : MonoBehaviour
 {
-    public Vector3[] destinos; 
-    public Quaternion[] rotaciones; 
+    public Vector3[] destinos;
+    public Quaternion[] rotaciones;
     public float duracion;
+    public float jumpPower = 2.0f;
+    public int numJumps = 1;
     public GameObject[] legos;
+    public Button moveButton; // Referencia al botón
 
-    private int currentLegoIndex = 0; 
+    private int currentLegoIndex = 0;
+    private bool isMoving = false; // Bandera para controlar el movimiento
+
+    void Start()
+    {
+        // Asignar el método Move al botón
+        moveButton.onClick.AddListener(Move);
+    }
 
     public void Move()
     {
-        if (currentLegoIndex < legos.Length)
+        if (!isMoving && currentLegoIndex < legos.Length)
         {
             StartCoroutine(MoveLegoOneByOne());
         }
@@ -23,15 +34,23 @@ public class movimientos : MonoBehaviour
 
     private IEnumerator MoveLegoOneByOne()
     {
+        isMoving = true; // Inicia el movimiento
+        moveButton.interactable = false; // Desactiva el botón para evitar más movimientos
 
-        legos[currentLegoIndex].transform.DOMove(destinos[currentLegoIndex], duracion);
+        // Mueve el objeto actual en un arco y rota
+        legos[currentLegoIndex].transform.DOJump(destinos[currentLegoIndex], jumpPower, numJumps, duracion);
         legos[currentLegoIndex].transform.DORotateQuaternion(rotaciones[currentLegoIndex], duracion);
 
-
+        // Espera hasta que el movimiento y la rotación terminen
         yield return new WaitForSeconds(duracion);
 
-        
+        // Incrementa el índice para mover el siguiente objeto en la próxima pulsación
         currentLegoIndex++;
-        Debug.Log("pieza actual" + currentLegoIndex);
+        Debug.Log("Pieza actual: " + currentLegoIndex);
+
+        isMoving = false; // Termina el movimiento
+        moveButton.interactable = true; // Reactiva el botón para permitir más movimientos
     }
 }
+
+
